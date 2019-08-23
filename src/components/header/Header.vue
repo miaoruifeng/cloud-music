c4c3905b0d4e0b3b6ef5e3501bf51a58c0c526af
miaoruifeng <mrfnet@163.com>
2019-08-23 11:31:45 +0800

    project init

---
+++ src/components/header/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Cloud Music</div>
+</template>
+
+<script>
+export default {
+  name: 'MHeader'
+}
+</script>
+
+<style lang="stylus" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+
+import 'stylus/common.styl'
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)
 
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
