7597efe4df246246ce706c4d110552d57c16f74f
miaoruifeng <mrfnet@163.com>
2019-09-02 15:20:51 +0800

    get singerDetail data

--- src/components/singer-detail/SingerDetail.vue
+++ src/components/singer-detail/SingerDetail.vue
@@ -6,15 +6,43 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { getSingerDetail } from 'api/singer'
+import { createSong } from 'common/js/song'
 export default {
   name: 'SingerDetail',
+  data () {
+    return {
+      songs: []
+    }
+  },
   computed: {
     ...mapGetters([
       'singer'
     ])
   },
+  methods: {
+    _getSingerDetail () {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+      }
+      getSingerDetail(this.singer.id).then((res) => {
+        this.songs = this._nomalizeSong(res.data.list)
+        console.log(this.songs)
+      })
+    },
+    _nomalizeSong (list) {
+      let ret = []
+      list.forEach((item) => {
+        let { musicData } = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
+  },
   created () {
-    console.log(this.singer)
+    this._getSingerDetail()
   }
 }
 </script>
