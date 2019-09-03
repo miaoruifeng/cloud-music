6ad36e9b5a6904fb98473beb7bf83c5f4c3bfb04
miaoruifeng <mrfnet@163.com>
2019-09-03 16:35:17 +0800

    add switch normal-mini animation

--- src/components/player/Player.vue
+++ src/components/player/Player.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="player" v-show="playList.length > 0">
-    <transition name="normal">
+    <transition
+      name="normal"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @leave="leave"
+      @after-leave="afterLeave"
+    >
       <div class="normal-player" v-show="fullScreen">
         <div class="bg-wrapper">
           <img class="bg-img" :src="currentSong.image"/>
@@ -14,7 +20,7 @@
         </div>
         <div class="middle">
           <div class="middle-l">
-            <div class="cd-wrapper">
+            <div class="cd-wrapper" ref="cdWrapper">
               <div class="cd">
                 <img class="cd-img" :src="currentSong.image"/>
               </div>
@@ -68,6 +74,11 @@
 
 <script>
 import { mapGetters, mapMutations } from 'vuex'
+import animations from 'create-keyframe-animation'
+import { prefixStyle } from 'common/js/dom'
+
+const transform = prefixStyle('transform')
+
 export default {
   name: 'Player',
   computed: {
@@ -78,11 +89,68 @@ export default {
     ])
   },
   methods: {
-    back  () {
+    /* normal - mini播放器之前的切换 */
+    // 点击左上角按钮切换到mini播放器
+    back () {
       this.setFullScreen(false)
     },
+    // 点击mini播放器切换到全屏播放
     openFullScreen () {
       this.setFullScreen(true)
+    },
+    // normal - mini切换动画
+    enter (el, done) {
+      const {x, y, scale} = this._getPosAndScale()
+      let animation = {
+        0: {
+          transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+        },
+        60: {
+          transform: `translate3d(0,0,0) scale(1.1)`
+        },
+        100: {
+          transform: `translate3d(0,0,0) scale(1)`
+        }
+      }
+      animations.registerAnimation({
+        name: 'move',
+        animation,
+        presets: {
+          duration: 400,
+          easing: 'linear'
+        }
+      })
+      animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+    },
+    afterEnter () {
+      animations.unregisterAnimation('move')
+      this.$refs.cdWrapper.style.animation = ''
+    },
+    leave (el, done) {
+      this.$refs.cdWrapper.style.transition = 'all 0.4s'
+      const {x, y, scale} = this._getPosAndScale()
+      this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+      this.$refs.cdWrapper.addEventListener('transitionend', done)
+    },
+    afterLeave () {
+      this.$refs.cdWrapper.style.transition = ''
+      this.$refs.cdWrapper.style[transform] = ''
+    },
+    // normal - mini之间切换，获取初始位置和大小方法
+    _getPosAndScale () {
+      const targetWidth = 40
+      const paddingLeft = 32
+      const paddingBottom = 30
+      const paddingTop = 80
+      const width = window.innerWidth * 0.8
+      const scale = targetWidth / width
+      const x = -(window.innerWidth / 2 - paddingLeft)
+      const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+      return {
+        x,
+        y,
+        scale
+      }
     },
     ...mapMutations({
       setFullScreen: 'SET_FULL_SCREEN'
